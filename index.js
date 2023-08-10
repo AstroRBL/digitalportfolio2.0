1afa06767128f47aad600e29ff373854a9d867d6
Aiman Suliman <125767562+ZaymanDev@users.noreply.github.com>
2023-08-10 23:57:32 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+const hamburger = document.querySelector(".hamburger");
+const close = document.querySelector(".close-ham");
+const navList = document.querySelector(".nav ul");
+
+hamburger.addEventListener("click", () => {
+  navList.style.display = "block";
+  hamburger.style.display = "none";
+  close.style.display = "block";
+});
+close.addEventListener("click", function () {
+  navList.style.display = "none";
+  hamburger.style.display = "block";
+  close.style.display = "none";
+});
+
 document.addEventListener("click", function (e) {
   if (
     e.target.tagName === "A" &&
@@ -8,6 +23,7 @@ document.addEventListener("click", function (e) {
     const targetElement = document.getElementById(targetId);
 
     if (targetId === "home") {
+      // Add this condition for the "Home" link
       window.scrollTo({
         top: 0,
         behavior: "smooth",
